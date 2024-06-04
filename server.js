5dcdc7be7aa6d465f2a6b65c2dbc82c7092ced6a
chenji <chenji53@crland.com.cn>
2024-06-04 14:11:23 +0800

    server.js 初始化

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+  res.end('网站正在建设中!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log('Server running at http://1.12.237.206/');
+});
